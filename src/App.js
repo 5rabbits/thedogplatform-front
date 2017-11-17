ec47094a9516c19d0d7372dccc473e3b0c67b6c0
Unknown <haruhixkti@gmail.com>
2017-11-17 17:44:19 -0300

    Se creo el formulario con los props correspondientes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import {Button, Icon} from 'react-materialize'
 import './App.css';
 
-import CardPublish from './components/CardPublish/CardPublish';
 
 class App extends Component {
   render() {
@@ -13,9 +12,31 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Button waves='light'>EDIT ME<Icon left>save</Icon></Button>
+
+        <CardPublish> </CardPublish>
+      </div>
+
+    );
+  }
+}
+
+export default App;
+
+import logo from './logo.svg';
+import {Button, Icon} from 'react-materialize'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
 
         <Button waves='light'>EDIT ME<Icon left>save</Icon></Button>
 
--- src/components/FormDog/FormDog.js
+++ src/components/FormDog/FormDog.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import {Input , Row, Dropdown, Button, Icon, NavItem} from  'react-materialize'
 
 class FormDog extends Component {
+		constructor(props) {
+	    super(props);
+	    this.state = {dogName: '', aggressiveness: '', dogAge: '', dogColor: '', dogRace: '',dogSize: ''};
+
+	    this.handleChange = this.handleChange.bind(this);
+	    this.sendInformation = this.sendInformation.bind(this);
+
+	}
+	  handleChange(event) {
+  			  //this.setState({value: event.target.value});
+  			  this.state.aggressiveness = event.target.value
+
+  		}
+  	sendInformation(event){
+  		event.preventDefault();
+    		console.log('The link was clicked.');
+  	}
+  	
     render(){
         return(
-
-            <div className='FormDog-description'>
-            Prueba FormDog
-            </div>
+       <div className='FormDog-description'>
+	     	<Row>
+				<Input placeholder="Ingrese el nombre del perro perdido" s={12} label="Nombre*" value = {this.state.dogName} />
+							
+				<Input s={12} type='select' label="Nivel de Agresividad" onChange={this.handleChange}  >
+					<option value='1'> Bajo  </option>
+					<option value='2'> Medio </option>
+					<option value='3'> Alto  </option>
+				</Input>
+				
+				<Input placeholder="Ingrese la edad del perro perdido"  s={6} label="Edad" value ={this.state.dogAge} />
+				<Input placeholder="Ingrese el color del perro perdido" s={6} label="Color"  value ={this.state.dogColor}/>
+				<Input placeholder="Ingrese la raza del perro perdido" 	s={6} label="Raza"  value ={this.state.dogRace}/>
+				<Input placeholder="Ingrese el tamaño del perro perdido"s={6} label="Tamaño" value ={this.state.dogSize} />
+				
+			</Row>
+		<Button waves='teal' node='a' onClick = {this.sendInformation} > Registrar perro  </Button>
+        </div>
 
         );
     }
